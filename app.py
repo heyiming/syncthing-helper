0a0833bfa681afb9f3445d39a55aad3cea4d6ef4
Yiming He <heyimingfrog@gmail.com>
2023-01-25 15:53:59 -0500

    Making the move

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-import asyncio, os, requests
+import asyncio
+import os
+import requests
+import shutil
 
-FREQ = int(os.getenv("FREQ", 10))
+FREQ = int(os.getenv("FREQ", 60))
 API_KEY = os.getenv("API_KEY", "Kns9wdjHcCCZ5W9NkX7mNYMkjuuJzqYi")
 IP_ADDRESS = os.getenv("IP_ADDRESS", "192.168.1.50")
 PORT = os.getenv("PORT", "8384")
+SYNC_DIR = "/mnt/sync"
+COMPLETION_DIR = "/mnt/completion"
 
 async def periodic():
     while True:       
@@ -27,8 +32,20 @@ def syncTask():
         print(f"Sycning in progress: {completion}")
         return
 
-    
-    
+    if not os.path.exists(SYNC_DIR):
+        print(f"SYNC_DIR {SYNC_DIR} does not exist")
+        return
+
+    if not os.path.exists(COMPLETION_DIR):
+        print(f"COMPLETION_DIR {COMPLETION_DIR} does not exist")
+        return
+
+    for entry in os.scandir(SYNC_DIR):
+        if entry.is_dir and entry.name == ".stfolder":
+            continue
+        print(f"Moving {entry} to DST")
+        shutil.move(entry.path, COMPLETION_DIR)
+
 
 
 if __name__ == "__main__":
