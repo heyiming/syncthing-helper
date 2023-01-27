93bf5ef40a619ad39a5906549d94e442b927af74
Yiming He <heyimingfrog@gmail.com>
2023-01-27 14:05:44 -0500

    Logging

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+#!/usr/bin/env python
+
 import asyncio
 import os
 import requests
 import shutil
+import logging
+
+# logging setup
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 FREQ = int(os.getenv("FREQ", 60))
-API_KEY = os.getenv("API_KEY", "Kns9wdjHcCCZ5W9NkX7mNYMkjuuJzqYi")
+API_KEY = os.getenv("API_KEY")
 IP_ADDRESS = os.getenv("IP_ADDRESS", "192.168.1.50")
 PORT = os.getenv("PORT", "8384")
 SYNC_DIR = "/mnt/sync"
@@ -15,35 +27,38 @@ async def periodic():
         try: 
             syncTask()
         except Exception as e:
-            print(f"Error when running syncTask: {e}")
+            logger.error(f"Error when running syncTask: {e}")            
         await asyncio.sleep(FREQ)
 
 def syncTask():
-    print("Syncing")
-    url = f"http://{IP_ADDRESS}:{PORT}/rest/db/completion"    
+    logger.info("Checking")
+    url = f"http://{IP_ADDRESS}:{PORT}/rest/db/completion"
+    if not API_KEY:
+        logger.error(f"API_KEY is unset")
+        return        
     response = requests.get(url, headers={"X-API-KEY":API_KEY})
     if response.status_code != 200:
-        print(f"Request failed with status code {response.status_code}")
+        logger.error(f"Request failed with status code {response.status_code}")        
         return
 
     status = response.json()
     completion = status["completion"]
     if completion != 100:
-        print(f"Sycning in progress: {completion}")
+        logger.info(f"Sycning in progress: {completion} %")
         return
 
     if not os.path.exists(SYNC_DIR):
-        print(f"SYNC_DIR {SYNC_DIR} does not exist")
+        logger.info(f"SYNC_DIR {SYNC_DIR} does not exist")
         return
 
     if not os.path.exists(COMPLETION_DIR):
-        print(f"COMPLETION_DIR {COMPLETION_DIR} does not exist")
+        logger.info(f"COMPLETION_DIR {COMPLETION_DIR} does not exist")
         return
 
     for entry in os.scandir(SYNC_DIR):
         if entry.is_dir and entry.name == ".stfolder":
             continue
-        print(f"Moving {entry} to DST")
+        logger.info(f"Moving {entry} to DST")
         shutil.move(entry.path, COMPLETION_DIR)
 
 
